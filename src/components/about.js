c5d595f180a30134e143b848e32f5bff23c035c1
Dylan Giliberto <dylangiliberto@gmail.com>
2023-06-24 19:52:22 -0700

    Final commit

--- src/components/about.js
+++ src/components/about.js
@@ -4,17 +4,17 @@ export default function About(){
             <div className='column'>
                 <h1>Hey!</h1>
                 <p className='paragraph'>
-                    I'm Dylan, A Computer Science student at the University of La Verne
+                    I'm Dylan, I recently graduated from the University of La Verne with a bachelors degree in Computer Science
                 </p>
                 <p className='paragraph'>
-                    I'll be graduating May 2023, and am looking to start a career in either Software Develeopment/Engineering or Data Science!
+                    I am looking to start a career in either Software Develeopment/Engineering or Data Science!
                 </p>
                 <p className='paragraph'>
                     I went to Burbank High School from 2015 to 2019. I became interested in Computer Science after taking AP Computer Science A 
                     in 2018, and decided that I would pursue my degree in CS after high school!
                 </p>
                 <p className='paragraph'>
-                    <i>This site is still a work in progress (: Thanks!</i>
+                    <i>This site is still a work in progress (:</i>
                 </p>
             </div>
             <div className='column'>
